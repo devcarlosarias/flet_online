4b6c585e3d2ea2b9ae4800c1c7a606a5df419e32
CARLOS ANTONIO ANGULO ARIAS <130503712+vjcarlosedit@users.noreply.github.com>
2024-06-04 20:10:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,30 +2,14 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.title = "Flet counter example"
+    page.title = "Sistema Posgrado"
     page.theme_mode = ft.ThemeMode.LIGHT
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
-
-    def minus_click(e):
-        txt_number.value = str(int(txt_number.value) - 1)
-        page.update()
-
-    def plus_click(e):
-        txt_number.value = str(int(txt_number.value) + 1)
-        page.update()
-
-    page.add(
-        ft.Row(
-            [
-                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
-                txt_number,
-                ft.IconButton(ft.icons.ADD, on_click=plus_click),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        )
-    )
-
+     appBar = ft.AppBar(title=ft.Text("Sistema De Adminstración"), 
+                       center_title=True, 
+                       bgcolor="green", 
+                       color="white")
+    page.appbar = appBar
 
 ft.app(main, view=ft.AppView.WEB_BROWSER)
